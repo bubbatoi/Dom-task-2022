fb3949eb301d4e1c540efc0f681551c16f0e9b0a
bubbatoi <bub98@hotmail.co.uk>
2022-05-17 19:51:52 +0100

    magic number
    fixed magic number task

--- main.js
+++ main.js
@@ -5,8 +5,20 @@ function roundUp(x) {
 
 function CalculateMagic(a, b) {
     // add your code here
-    
-    
+  let number = a + b + b;
+  number = number + 90;
+  number = number / 10;  
+  if (number % 5 === 0){
+      number = number - 8;
+  }
+      else {
+          number = number + 9;
+      }
+  
+number = number * number;
+let g = number / 3;
+
+return roundUp(g);
     // remember to call roundUp() with your result before returning
     // return roundUp(g);
 }
